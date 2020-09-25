632bca4d455412b96e19b2c4630e91875d38a790
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2020-09-25 10:35:18 +0700

    [UPDATE] Jan tag

--- hacktoberfest2020/src/App.js
+++ hacktoberfest2020/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Button from './components/Button';
 import Navbar from './components/Navbar';
+import Jan from './components/Jan';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Button />
+      <Jan />
     </div>
   );
 }
