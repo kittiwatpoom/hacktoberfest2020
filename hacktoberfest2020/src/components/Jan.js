a981855a09e96741025d42b346a5c6ec407f0ff3
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2020-09-25 10:34:40 +0700

    [ADD] text happy

---
+++ hacktoberfest2020/src/components/Jan.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Jan extends Component {
+    render() {
+        return (
+            <div>
+                happy
+            </div>
+        )
+    }
+}
