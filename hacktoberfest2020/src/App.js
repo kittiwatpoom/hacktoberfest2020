74e8a40d04c484bf3a348c621086c052b9512dff
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2020-10-01 14:49:29 +0700

    [UPDATE] march and april tag

--- hacktoberfest2020/src/App.js
+++ hacktoberfest2020/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Jan from './components/Jan';
+import March from './components/March';
+import April from './components/April';
 
 function App() {
   return (
     <div className="App">
       <Jan />
+      <March />
+      <April />
     </div>
   );
 }
