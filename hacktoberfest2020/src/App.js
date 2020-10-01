2a46fed31e1a9aef63f87221ed43e089237ee6c9
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2020-10-01 14:57:02 +0700

    [UPDATE] june and july

--- hacktoberfest2020/src/App.js
+++ hacktoberfest2020/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Jan from './components/Jan';
 import March from './components/March';
 import April from './components/April';
+import June from './components/June';
+import July from './components/July';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Jan />
       <March />
       <April />
+      <June />
+      <July />
     </div>
   );
 }
