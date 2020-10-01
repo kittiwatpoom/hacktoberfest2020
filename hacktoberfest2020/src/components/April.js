0197bf751464db6b034e32ca73160d607d647e55
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2020-10-01 14:49:00 +0700

    [ADD] april txt

---
+++ hacktoberfest2020/src/components/April.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class April extends Component {
+    render() {
+        return (
+            <div>
+                Sleep well
+            </div>
+        )
+    }
+}
